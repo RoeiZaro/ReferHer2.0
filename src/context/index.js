ff6331d341e4219fab2e59f8e9b9a75dce92c126
OrrFarber <Orr.farber@tichonet.com>
2023-03-08 14:44:16 +0200

    try uploading photo again :)

---
+++ src/context/index.js
@@ -0,0 +1,77 @@
+const obj = {
+  media_details: {
+    width: 1114,
+    height: 1122,
+    file: "2023/03/Screenshot-2023-03-08-at-10.29.16.png",
+    filesize: 1435112,
+    sizes: {
+      medium: {
+        file: "Screenshot-2023-03-08-at-10.29.16-298x300.png",
+        width: 298,
+        height: 300,
+        filesize: 109822,
+        mime_type: "image/png",
+        source_url:
+          "https://referher.co/wp-content/uploads/2023/03/Screenshot-2023-03-08-at-10.29.16-298x300.png",
+      },
+      large: {
+        file: "Screenshot-2023-03-08-at-10.29.16-1017x1024.png",
+        width: 1017,
+        height: 1024,
+        filesize: 835717,
+        mime_type: "image/png",
+        source_url:
+          "https://referher.co/wp-content/uploads/2023/03/Screenshot-2023-03-08-at-10.29.16-1017x1024.png",
+      },
+      thumbnail: {
+        file: "Screenshot-2023-03-08-at-10.29.16-150x150.png",
+        width: 150,
+        height: 150,
+        filesize: 32710,
+        mime_type: "image/png",
+        source_url:
+          "https://referher.co/wp-content/uploads/2023/03/Screenshot-2023-03-08-at-10.29.16-150x150.png",
+      },
+      medium_large: {
+        file: "Screenshot-2023-03-08-at-10.29.16-768x774.png",
+        width: 768,
+        height: 774,
+        filesize: 542700,
+        mime_type: "image/png",
+        source_url:
+          "https://referher.co/wp-content/uploads/2023/03/Screenshot-2023-03-08-at-10.29.16-768x774.png",
+      },
+      "tp-image-grid": {
+        file: "Screenshot-2023-03-08-at-10.29.16-700x700.png",
+        width: 700,
+        height: 700,
+        filesize: 466530,
+        mime_type: "image/png",
+        source_url:
+          "https://referher.co/wp-content/uploads/2023/03/Screenshot-2023-03-08-at-10.29.16-700x700.png",
+      },
+      full: {
+        file: "Screenshot-2023-03-08-at-10.29.16.png",
+        width: 1114,
+        height: 1122,
+        mime_type: "image/png",
+        source_url:
+          "https://referher.co/wp-content/uploads/2023/03/Screenshot-2023-03-08-at-10.29.16.png",
+      },
+    },
+    image_meta: {
+      aperture: "0",
+      credit: "",
+      camera: "",
+      caption: "",
+      created_timestamp: "0",
+      copyright: "",
+      focal_length: "0",
+      iso: "0",
+      shutter_speed: "0",
+      title: "",
+      orientation: "0",
+      keywords: [],
+    },
+  },
+};
--- src/context/postMongo.js
+++ src/context/postMongo.js
@@ -1,73 +1,114 @@
 import { Button, View, Text, Image } from "react-native";
 import { API_URL } from "@env";
 import { useState } from "react";
+import axios from "axios";
 
 function PostMongo() {
   let postArr = [];
-  const [testPost, setTestPost] = useState([]);
-  async function GetPostsFromAPI() {
+  const [image, setImage] = useState("");
+  const [post, setPost] = useState([]);
+
+  async function getPosts() {
     try {
-      const response = await fetch(`${API_URL}/posts?_fields`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-      });
-      //   console.log(response);
-      const data = await response.json();
-      //   console.log("data ", data);
-      data?.map(async (post) => {
-        const image = await fetch(`${API_URL}/media/${post.featured_media}`, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-          },
-        });
-        image.json().then((imgUrl) => {
-          imgUrl && setTestPost([...postArr, { post, image: imgUrl }]);
-        });
-        // postArr.push({
-        //   post,
-        //   image: imageresponse.media_details?.sizes.full.source_url,
-        // });
-      });
-      // console.log(postArr, "1");
-      // console.log("data: ", data);
+      const { data: person } = await axios.get(
+        `${API_URL}/posts?_fields=id,title,author,featured_media`
+      );
+      // setPost(person);
+      console.log(person);
       try {
-        const tomongo = await fetch("http://192.168.50.51:8002/updateposts", {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(postArr),
-        }).catch(function (err) {
-          console.log(err);
-        });
-        console.log(await tomongo.json());
-      } catch (error) {
-        console.log(error);
+        const peopleWithImages = await Promise.all(
+          person.map(async (post) => {
+            const { data: image } = await axios.get(
+              `${API_URL}/media/${post.featured_media}`
+              // `${API_URL}/media?parent=${post.id}`
+            );
+            return {
+              id: post.id,
+              title: post.title,
+              author: post.author,
+              image: image.media_details.sizes.full.source_url,
+            };
+          })
+        );
+        console.log(peopleWithImages);
+        // console.log(images);
+      } catch (err) {
+        console.log("MapError", err);
       }
-    } catch (error) {
-      console.log(error);
+    } catch (err) {
+      console.log("MainError", err);
     }
   }
+
+  // async function GetPostsFromAPI() {
+  //   try {
+  //     const response = await fetch(
+  //       `${API_URL}/posts?_fields=id,title,author,featured_media`,
+  //       {
+  //         method: "GET",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //           Accept: "application/json",
+  //         },
+  //       }
+  //     );
+  //     const data = await response.json();
+
+  //     data?.map(async (post) => {
+  //       const image = await fetch(`${API_URL}/media/${post.featured_media}`, {
+  //         method: "GET",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //           Accept: "application/json",
+  //         },
+  //       });
+  //       image.json().then((imgUrl) => {
+  //         imgUrl && setTestPost([...postArr, { post, image: imgUrl }]);
+  //       });
+  //       console.log(image, "2");
+  //       // postArr.push({
+  //       //   post,
+  //       //   image: imageresponse.media_details?.sizes.full.source_url,
+  //       // });
+  //     });
+  //     // console.log("data: ", data);
+  //     try {
+  //       const tomongo = await fetch("http://172.20.10.5:8002/updateposts", {
+  //         method: "POST",
+  //         headers: {
+  //           Accept: "application/json",
+  //           "Content-Type": "application/json",
+  //         },
+  //         body: JSON.stringify(postArr),
+  //       }).catch(function (err) {
+  //         console.log(err);
+  //       });
+  //       console.log(await tomongo.json());
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
   // console.log(JSON.parse(testPost.post));
-  console.log(postArr);
+  // console.log(testPost, "1");
   return (
     <View>
-      <Button onPress={() => GetPostsFromAPI()} title="Press Me">
+      <Button onPress={() => getPosts()} title="Press Me">
         Press Me
       </Button>
-      {/* <Text>
-        {testPost?.map((post) => {
-          // alert(post.id);
-          return (
-            <View key={post.id}>
+      {/* <Text> 
+       {peopleWithImages?.map((data) => {
+          return ( 
+            <View key={data.id}>
               <View className>
-               
+                <Text>{data.title}</Text>
+                <Text>{data.title}</Text>
+                <Image
+                  className="w-14 h-14 rounded-full border-2 ml-auto border-blue-300"
+                  source={{ uri: data.image }}
+                />
               </View>
             </View>
           );
